58bdfc6c25a7d40bbfe2b60d026995331460d9b3
HaiDang <dangtranhai628@gmail.com>
2022-08-26 19:44:00 +0700

    Change checkbox button to toggle button in Settings page

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Input } from '../../components';
 import { useLocalStorage } from '../../hooks';
+import styled from 'styled-components';
 
 const Settings = (props) => {
     const [theme, setTheme] = useLocalStorage('data-theme', 'light');
@@ -20,14 +21,11 @@ const Settings = (props) => {
                     <Card title="Settings">
                         <div>
                             <label style={{ marginRight: '6px' }} htmlFor="theme">
-                                {theme === 'light' ? 'Dark' : 'Light'} Theme
+                                Dark Theme
                             </label>
-                            <input
-                                type="checkbox"
-                                id="theme"
-                                value={theme}
-                                onClick={() => handleChangeTheme()}
-                            />
+                            <Toggle theme={theme} onClick={() => handleChangeTheme()}>
+                                <Circle theme={theme} />
+                            </Toggle>
                         </div>
                         <div>
                             <p>Change default color: </p>
@@ -44,3 +42,37 @@ const Settings = (props) => {
 Settings.propTypes = {};
 
 export default Settings;
+
+const Toggle = styled.div`
+    display: inline-block;
+    padding: 0.25rem;
+    background-color: ${(props) =>
+        props.theme === 'dark' ? 'var(--primary-color)' : 'rgba(15, 23, 42, 0.1)'};
+    transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+        transform;
+    transition-duration: 200ms;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    width: 2.5rem;
+    height: 1.5rem;
+    border-radius: 9999px;
+    box-shadow: var(--box-shadow);
+    pointer-events: auto;
+    cursor: pointer;
+`;
+
+const Circle = styled.div`
+    background-color: #ffffff;
+    transition-property: background-color, border-color, color, fill, stroke, opacity, box-shadow,
+        transform;
+    transition-duration: 200ms;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+    width: 1rem;
+    height: 1rem;
+    border-radius: 9999px;
+    box-shadow: var(--box-shadow);
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
+    transform: ${(props) =>
+        props.theme === 'dark'
+            ? 'translate(1rem, 0) rotate(0) skewX(0) skewY(0) scaleX(1) scaleY(1)'
+            : '0'};
+`;
